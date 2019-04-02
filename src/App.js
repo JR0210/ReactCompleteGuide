ecd485318c0d41a1db3bba9b10d93c5b79d6f21d
Jacob Robinson <Jacob.Robinson@firstservesolutions.co.uk>
2019-04-02 16:25:21 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Jacob', age: 22 },
+      { name: 'Ben', age: 24 },
+      { name: 'Peggy', age: 31 }
+    ],
+    otherState: 'This has a value',
+    showPersons: false
+  }
+
+  switchNameHandler = (newName) => {
+    // DON'T EDIT STATE DIRECTLY this.state.persons[0].name = "Jake";
+    this.setState({
+      persons: [
+        { name: newName, age: 22 },
+        { name: 'Ben', age: 24 },
+        { name: 'Peggy', age: 31 }
+      ]
+    });
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Jacob', age: 22 },
+        { name: event.target.value, age: 24 },
+        { name: 'Peggy', age: 31 }
+      ]
+    })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow})
+  }
+  
   render() {
+    let buttonStyle = {
+      cursor: 'pointer',
+      borderRadius: '5px',
+      marginBottom: '10px'
+    }
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            click={() => this.switchNameHandler('Jacob')}>
+            My Hobbies: Racing
+        </Person>
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            changed={this.nameChangedHandler} />
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      )
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I'm a React App</h1>
+        <button style={buttonStyle} onClick={this.togglePersonsHandler}>Switch Name</button>
+        {persons}
       </div>
     );
   }
